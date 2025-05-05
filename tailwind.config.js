f998ada801f62dbdc3d7b52a4be2f99b99fdad42
HalfSweet <HalfSweet@HalfSweet.cn>
2025-05-05 18:12:45 +0800

    init: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"], // 添加您想使用的主题
+  },
+}
