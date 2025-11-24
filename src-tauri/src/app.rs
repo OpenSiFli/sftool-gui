fd464c9168bc827c514cf64de9b40a16bb5d6ca5
HalfSweet <HalfSweet@HalfSweet.cn>
2025-11-24 17:45:01 +0800

    feat: 添加更新功能

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -9,6 +9,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             app.manage(Mutex::new(AppState::default()));
