32c0e69593caf89e4451e118fe1d73ae46ba247d
bowenxie <bowenxie@sifli.com>
2025-11-25 11:10:47 +0800

    添加文档

---
+++ docs/src/.vuepress/config.ts
@@ -0,0 +1,40 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/projects/sftool/",
+
+  locales: {
+    "/en/": {
+      lang: "en-US",
+      title: "sftool-gui",
+      description: "sftool-gui",
+    },
+    "/": {
+      lang: "zh-CN",
+      title: "sftool-gui",
+      description: "sftool-gui",
+    },
+  },
+
+  theme,
+
+  head: [
+    [
+      'script',
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement(\"script\");
+        hm.src = \"https://hm.baidu.com/hm.js?b12a52eecef6bedee8b8e2d510346a6e\";
+        var s = document.getElementsByTagName(\"script\")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ]
+  ],
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
---
+++ docs/src/FAQs/README.md
@@ -0,0 +1,6 @@
+---
+title: 常见问题
+icon: fa-solid fa-wrench
+---
+
+## 敬请期待
