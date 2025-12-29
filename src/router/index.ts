2020cdf86bffb53ebae1d0bf316d4e71c7d7700e
HalfSweet <HalfSweet@HalfSweet.cn>
2025-12-29 21:16:23 +0800

    feat(router): add read-flash route
    - Import ReadFlashView component
    - Add /read-flash route path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import SettingsView from '../views/SettingsView.vue'
 import WriteFlashView from '../views/WriteFlashView.vue'
+import ReadFlashView from '../views/ReadFlashView.vue'
 import AboutView from '../views/AboutView.vue'
 import LogWindowView from '../views/LogWindowView.vue'
 
 const routes = [
-  {path: '/', redirect: '/write-flash'},
-  {path: '/setting', name: 'Setting', component: SettingsView},
-  {path: '/write-flash', name: 'WriteFlash', component: WriteFlashView},
-  {path: '/about', name: 'About', component: AboutView},
-  {path: '/log-window', name: 'LogWindow', component: LogWindowView},
+  { path: '/', redirect: '/write-flash' },
+  { path: '/setting', name: 'Setting', component: SettingsView },
+  { path: '/write-flash', name: 'WriteFlash', component: WriteFlashView },
+  { path: '/read-flash', name: 'ReadFlash', component: ReadFlashView },
+  { path: '/about', name: 'About', component: AboutView },
+  { path: '/log-window', name: 'LogWindow', component: LogWindowView },
 ]
 
 const router = createRouter({
