22929d921bc6f59fccfb3c8cd3c65c92b9c34886
HalfSweet <halfsweet@halfsweet.cn>
2025-12-30 11:17:59 +0800

    chore(lint): add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'docs/**', 'src-tauri/**', 'sftool-lib/**'],
+  },
+  {
+    ...js.configs.recommended,
+    languageOptions: {
+      ...js.configs.recommended.languageOptions,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  prettier,
+];
