d3e5701cfe5960b28db6d0f773600c03f189fd5d
HalfSweet <halfsweet@halfsweet.cn>
2026-01-06 15:54:35 +0800

    test: 1

--- src/utils/progressHandler.ts
+++ src/utils/progressHandler.ts
@@ -284,6 +284,7 @@ export class ProgressHandler {
   private formatBytes(bytes: number | undefined): string {
     if (!bytes || bytes === 0) return '0 B';
     const k = 1024;
+    
     const sizes = ['B', 'KB', 'MB', 'GB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(1)) + ' ' + sizes[i];
